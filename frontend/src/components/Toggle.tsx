8fc4f5d0b13fa6e3a0e1bf63e98dd43099208c28
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2022-01-13 23:10:57 +0100

    improve toggle

--- frontend/src/components/Toggle.tsx
+++ frontend/src/components/Toggle.tsx
@@ -11,8 +11,9 @@ export const Toggle: React.FC<ToogleProps> = ({ label, active, onChange }) => {
     onChange && onChange(!active);
   };
   return (
-    <button className="toggle" onClick={changeHandler}>
-      {label}:{active ? "Yes" : "All"}
+    <button className="button">
+      <input className="toggle" type="checkbox" onChange={changeHandler} />
+      {label}
     </button>
   );
 };
